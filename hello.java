1caaa8583be799a701fadcb3d5482972402088a9
Smruthul M S <97047561+Smruthul12@users.noreply.github.com>
2024-10-25 10:40:23 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Dum DUmM!"); 
+        System.out.println("Hello Dum DUm!"); 
     /*smee --url https://smee.io/YCuj4dUAz3hHM5do --path /github-webhook/ --port 8080*/
     }
 }
