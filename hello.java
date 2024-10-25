05def5969e2ddd0d1902c9c8e358ff4d6efab5b1
Smruthul M S <97047561+Smruthul12@users.noreply.github.com>
2024-10-25 10:35:14 +0530

    add hello.java

---
+++ hello.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Dum DUmM!"); 
+    /*smee --url https://smee.io/YCuj4dUAz3hHM5do --path /github-webhook/ --port 8080*/
+    }
+}
